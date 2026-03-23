2c3390887e6293cafc488007024de4c235cf40d5
LeTranVu00 <vult8073@ut.edu.vn>
2026-03-23 15:42:16 +0700

    oke

--- js/script.js
+++ js/script.js
@@ -46,10 +46,16 @@ btn.addEventListener("click", function () {
         } 
         else if (count === 50) {
             message.innerText = "Cố lên sắp tới rồi đó 😈";
+        }
+        else if (count === 60) {
+            message.innerText = "gần đến đích rồi 😈"; 
+        }
+        else if (count === 40) {
+            message.innerText = "cố lên 😈"; 
         } 
         else if (count < 30) {
             message.innerText = "khi nhấn đủ 30 lần thì bí mật sẽ được tiết lộ 😈";
-        } 
+        }
         else {
             message.innerText = "Nhanh lênnn 😈";
         }
